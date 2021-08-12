53c9b007af4f52a156522a49409dc68d1896839d
michaelma <16tm43@queensu.ca>
2021-08-12 12:40:08 -0700

    refine structure

--- train.py
+++ train.py
@@ -1,46 +1,12 @@
 import torch
-import torch.nn as nn
 import random
 from itertools import count
 import torch.nn.functional as F
 from tensorboardX import SummaryWriter
 import replay_memory
+from ddqn_model import QNetwork
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
-class QNetwork(nn.Module):
-    def __init__(self):
-        super(QNetwork, self).__init__()
-
-        self.fc1 = nn.Linear(3, 64)
-        self.relu = nn.ReLU()
-        self.leakey = nn.LeakyReLU()
-        self.fc_value = nn.Linear(64, 256)
-        self.fc_adv = nn.Linear(64, 256)
-
-        self.value = nn.Linear(256, 1)
-        self.adv = nn.Linear(256, 14)
-
-    def forward(self, state):
-        y = self.leakey(self.fc1(state))
-        value = self.leakey(self.fc_value(y))
-        adv = self.leakey(self.fc_adv(y))
-
-        value = self.value(value)
-        adv = self.adv(adv)
-
-        advAverage = torch.mean(adv, dim=1, keepdim=True)
-        Q = value + adv - advAverage
-
-        return Q
-
-    def select_action(self, state):
-        with torch.no_grad():
-            Q = self.forward(state)
-            action_index = torch.argmax(Q, dim=1)
-        return action_index.item()
-
 
 onlineQNetwork = QNetwork().to(device)
 targetQNetwork = QNetwork().to(device)
